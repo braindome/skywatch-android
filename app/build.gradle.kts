3e1b18693f9a75916c099f2797f9f5b11483e072
Antonio Saracino <antonio.saracino83@gmail.com>
2025-01-02 13:38:40 +0100

    Updated sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "se.braindome.skywatch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "se.braindome.skywatch"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
