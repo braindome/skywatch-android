9f8fa527f81e5912c7794aa2eb51d7e726628e39
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2025-02-11 01:59:26 +0100

    Added version catalog plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("nl.littlerobots.version-catalog-update") version "0.8.5"
     id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 
